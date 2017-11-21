97bbe5ef20d4edc328f61715c714304e6b1be1ed
MatanSilver <matansilver@gmail.com>
2017-11-21 16:29:34 -0500

    added copy function test

---
+++ main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestCopy(t *testing.T) {
+	out, err := os.Create("./testfile.txt")
+	defer out.Close()
+	if err != nil {
+		panic(err)
+	}
+	_, err = out.WriteString("this is a test\n")
+	out.Sync()
+
+	Copy("./testfile.txt", "./newtestfile.txt")
+
+	dat, err := ioutil.ReadFile("./newtestfile.txt")
+	if err != nil {
+		panic(err)
+	}
+	recovered_string := string(dat)
+	if "this is a test\n" != recovered_string {
+		t.Errorf("Recovered string doesn't match written string")
+	}
+}
